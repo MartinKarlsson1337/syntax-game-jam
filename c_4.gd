96d1bbed6c75597e2ecc686e79b615645ae55ea2
Eric Jansson <rapidall@hotmail.com>
2024-04-26 12:06:07 +0200

    refactor

--- c_4.gd
+++ c_4.gd
@@ -1,9 +1,7 @@
 extends bomb_base
 
-@onready var sprite = $animator
-
 func explode():
-	sprite.play("explosion_plus")
+	animator.play("explosion_plus")
 	if playerIsInExplosionArea:
 		playerExploded.emit(position)
-		
+
